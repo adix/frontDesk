99e1e66f0e3de964b4a575fa17ce23640e526559
adix <padhyeav@gmail.com>
2015-04-16 02:54:51 -0400

    Adding router.js
    Routing pages

---
+++ client/router.js
@@ -0,0 +1,17 @@
+/**
+Router.configure({
+	layoutTemplate:''
+})
+*/
+
+//check if logged in and reroute.
+Router.map(function(){
+	this.route('login',{path:'/'});
+	this.route('currentlyAt',{path:'/currentlyAt'});
+	this.route('hotelSelect',{path:'/hotelSelect'});
+	this.route('reqHistory',{path:'/reqHistory'});
+	this.route('preCheckin',{path:'/preCheckin'});
+	this.route('hotelServices',{path:'/hotelServices'});
+	this.route('roomNumber',{path:'/roomNumber'});
+
+})
