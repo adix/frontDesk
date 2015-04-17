08dfdeea8aaed19cea5d16ae4fc106f17adb03df
nilaypanchal <nilayspanchal@gmail.com>
2015-04-17 00:40:41 -0400

    Routing the correct way

--- client/login/login.js
+++ client/login/login.js
@@ -1,3 +1,11 @@
-Template.login.events({
-	
-})
+var oldUserId = undefined;
+
+Meteor.autorun(function() {
+  var newUserId = Meteor.userId();
+  if (oldUserId === null && newUserId) {
+    console.log('The user logged in');
+  } else if (newUserId === null && oldUserId) {
+    console.log('The user logged out');
+  } 
+  oldUserId = Meteor.userId();
+});
