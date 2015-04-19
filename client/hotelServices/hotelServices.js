ad0d770ac5bee36b5466b15da28d879456fd73f2
adix <padhyeav@gmail.com>
2015-04-18 20:31:00 -0400

    Modified Layout, Hotel Services displayed

--- client/hotelServices/hotelServices.js
+++ client/hotelServices/hotelServices.js
@@ -1,7 +1,7 @@
 Meteor.subscribe('hotels');
 
-Template.hotelServices.helpers({
-	services: function(){
+Template.serviceDisplay.helpers({
+	hotels: function(){
 		console.log("In the function");
 		console.log(Hotels.find().fetch());
 		return Hotels.find();		
