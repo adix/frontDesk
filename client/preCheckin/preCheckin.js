012b615dc99a660d819b8ddc9f9b9149c17e20e7
adix <padhyeav@gmail.com>
2015-04-23 15:00:14 -0400

    Distance from hotel found on click event

--- client/preCheckin/preCheckin.js
+++ client/preCheckin/preCheckin.js
@@ -12,31 +12,39 @@ Template.preCheckin.helpers({
 		return Session.get('eta');
 		console.log(Session.get('eta'));		
 	},
-	//fetch only users current hotel
+	//change fetch only users current hotel
 	hotels: function(){
-		console.log("In the function");
-		console.log(Hotels.find().fetch());
+		//console.log("In the function");
+		//console.log(Hotels.find().fetch());
 		return Hotels.find();		
 	}
 })
 
+
+//How to write small modular functions to do everything that you have to do.
 Template.preCheckin.events({	
 	'click .share-location': function(){
-		//Call to mehtod to get eta
-		Meteor.call("getETA", function(error, results) {
-   		console.log(results.data.rows[0].elements[0]); //results.data should be a JSON object
-   		// cant return as async call
-  		//return results.data.rows[0].elements[0];
-   		//setting session variable
-   		Session.set('eta',results.data.rows[0].elements[0])
-		});
+		var hotels = Hotels.find().fetch();
 		
-		console.log("Share Location clicked");		
+		//Currently gets address of first hotel. Need to change it to fetch current hotel from current user profile.
+		var hotelLat = hotels[0].address.lat;
+		var hotelLng = hotels[0].address.lng
+
+		//Call to mehtod to get eta
+		Meteor.call("getETA", hotelLat, hotelLng, function(error, results) {
+   			console.log(results.data.rows[0].elements[0]); //results.data should be a JSON object
+   			//Cant return as async call but can set it directly in the database
+  			//return results.data.rows[0].elements[0];
+   			//setting session variable
+   			Session.set('eta',results.data.rows[0].elements[0])
+		});
+
+		//console.log("Share Location clicked");		
 		//get location using mdg:location package
 		var location = Geolocation.latLng() || { lat: 0, lng: 0 };
 		var error = Geolocation.error;
-		console.log(location.lat);
-		console.log(location.lng);
+		//console.log(location.lat);
+		//console.log(location.lng);
 		//console.log(error);
 		//Set location to session variable
 		Session.set("location",location)
--- server/methods.js
+++ server/methods.js
@@ -1,8 +1,7 @@
 Meteor.methods({
     'getETA' : function (lat,lng) {
-        //this.unblock() causes subsequent methods to run in different fibers
-        //this.unblock();
-        //console.log(temp);
-        return  Meteor.http.call("GET", "https://maps.googleapis.com/maps/api/distancematrix/json?origins=41.43206,-81.38992&destinations=40.4450877,-79.9809263");        
+        //this.unblock() causes subsequent methods to run in different fibers. Dont know what the use of this is exactly.
+        this.unblock();
+        return  Meteor.http.call("GET", "https://maps.googleapis.com/maps/api/distancematrix/json?origins=41.43206,-81.38992&destinations="+lat+","+lng);        
     }
 });
