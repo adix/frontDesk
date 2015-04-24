3357a8775d9ad7629ce47540279c7ca732d28e33
adix <padhyeav@gmail.com>
2015-04-24 01:23:55 -0400

    Method to add precheckin service to hotel Service history array

--- server/methods.js
+++ server/methods.js
@@ -1,13 +1,23 @@
+//var ObjectID, require; 
+//require = __meteor_bootstrap__.require; 
+//ObjectID = require("mongodb").ObjectID;
+
 Meteor.methods({
     'getETA' : function (userLat,userLng,hotelLat,hotelLng) {
         //this.unblock() causes subsequent methods to run in different fibers. Dont know what the use of this is exactly.
         this.unblock();
         return  Meteor.http.call("GET", "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+userLat+","+userLng+"&destinations="+hotelLat+","+hotelLng);        
-    },    
-    'addUserLocation' : function(userLat, userLng) {
-    	//console.log("In assUserLocation");
-    	//console.log(userLat);
-    	//console.log(userLng);    	
-
-    }  
+    },
+    'updateUserEta' : function(eta){
+        console.log("IN the serverererere");
+        console.log(Meteor.user().profile.currentHotel.hotel_id);
+        //var id; id = new ObjectID(Meteor.user().profile.currentHotel.hotel_id); 
+        //console.log(id);
+        return Hotels.update({ _id: new Meteor.Collection.ObjectID(Meteor.user().profile.currentHotel.hotel_id)},
+            {$addToSet:{ServiceHistory:{user_id:Meteor.userId(),service_performed:{name: "PreCheckin",eta:{distance: eta.distance.text,duration: eta.duration.text},createdAt:Date()}}}}
+        );
+    },
+    //{ SomeUpdate: function(upd) { 
+    //   var id; id = new ObjectID(upd['_id']); 
+    //   return SomeDB.update({ _id: id }, { $set: { field: value } }, function(res) { return console.log(res); }); } }
 });
