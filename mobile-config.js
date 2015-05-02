992a8ae336a719e75d7d8a43d4f09a1e0fb62692
adix <padhyeav@gmail.com>
2015-05-02 11:33:15 -0400

    Icons and splash screens

--- mobile-config.js
+++ mobile-config.js
@@ -18,7 +18,7 @@ App.icons({
   //'android_ldpi': 'resources/icons/icon-36x36.png',
   //'android_mdpi': 'resources/icons/icon-48x48.png',
   //'android_hdpi': 'resources/icons/icon-72x72.png',
-  'android_xhdpi': 'resources/icons/icon-96x96.png'
+  'android_xhdpi': 'resources/icons/icon-96x96.gif'
 });
 
 App.launchScreens({
